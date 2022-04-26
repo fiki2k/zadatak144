f212e5eb2acb90651cb0a7292c1afe5428a9b218
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-04-26 09:32:41 +0200

    Add files via upload

---
+++ zad144.c
@@ -0,0 +1,37 @@
+// Program treba zamjenit vrijednosti brojeva pomocu pointera.
+
+#include<stdio.h>
+
+void zamjena(int *a,int *b,int *c);
+
+int main()
+{
+    int a, b, c;
+
+    printf("Unesi a : ");
+    scanf("%d",&a);
+    printf("\nUnesi b : ");
+    scanf("%d",&b);
+    printf("\nUnesi c: ");
+    scanf("%d",&c);
+
+    printf("\nVrijednosti prije zamjene: \n");
+    printf("\ta = %d\tb = %d\tc = %d\n",a,b,c);
+
+    zamjena(&a, &b, &c);
+
+    printf("\nVrijednosti nakon zamjene: \n");
+    printf("\ta = %d\tb = %d\tc = %d\n",a,b,c);
+
+    return 0;
+}
+void zamjena(int *a,int *b,int *c)
+{
+
+    int temp;
+
+    temp = *b;
+    *b = *a;
+    *a = *c;
+    *c = temp;
+}
